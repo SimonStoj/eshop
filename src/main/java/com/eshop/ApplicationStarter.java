190d9d2c75953d2f50d5f9c358815c4d9db6aeab
Simon <s.stojanovski@axeltra.com>
2018-02-13 15:49:38 +0100

    Connected with db

--- src/main/java/com/eshop/ApplicationStarter.java
+++ src/main/java/com/eshop/ApplicationStarter.java
@@ -1,12 +1,13 @@
 package com.eshop;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.eshop"})
 public class ApplicationStarter {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(ApplicationStarter.class, args);
     }
 
 }
---
+++ src/main/java/com/eshop/persistance/Product.java
@@ -0,0 +1,34 @@
+package com.eshop.persistance;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
