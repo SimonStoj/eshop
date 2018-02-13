21b2e5e77b75c3316f739cb498e7e16e0569464e
Simon <s.stojanovski@axeltra.com>
2018-02-13 14:42:21 +0100

    initial commit

---
+++ src/main/java/com/eshop/ApplicationStarter.java
@@ -0,0 +1,12 @@
+package com.eshop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+
+    }
+
+}
